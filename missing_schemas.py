15bbe8a006bd4cc1e3cf31abaa681798842e3cf4
nadias03 <71732510+nadias03@users.noreply.github.com>
2026-03-09 22:19:42 +0100

    add mar1 and mar2 implementation

--- missing_schemas.py
+++ missing_schemas.py
@@ -26,6 +26,64 @@ def MCAR(X, Y, missing_rate):
 
     return X, Y_obs
 
+def MAR1(X, Y, missing_rate):
+
+    """
+    Function implementing the Missing At Random (MAR1) schema in Y - assumes that the missing data mechanism depends only on a single explanatory variable.
+    In this implementation the "single explanatory variable" is always the first column.
+
+    Parameters:
+        X: pd.DataFrame - explanatory variables
+        Y: pd.Series - target variable
+        missing_rate: float - fraction of missing Y
+
+    Returns:
+        X: pd.DataFrame (unchanged)
+        Y_obs: pd.Series - Y with -1 where Y is missing
+    """
+
+    Y_obs = Y.copy()
+    n = len(Y)
+    n_missing= int(n * missing_rate)
+
+    feature = np.array(X.iloc[:, 0])
+    weights = feature - feature.min() + 1e-6
+    probabilities = weights / weights.sum()   # the bigger the value the higher probability it is going to be changed to missing - dependence on one feature
+
+    missing_indices = np.random.choice(a=n, size=n_missing, replace=False, p=probabilities)
+    Y_obs.iloc[missing_indices] = -1
+
+    return X, Y_obs
+
+def MAR2(X, Y, missing_rate):
+
+    """
+    Function implementing the Missing At Random (MAR2) schema in Y - assumes that the missing data mechanism depends on all explanatory variables.
+
+    Parameters:
+        X: pd.DataFrame - explanatory variables
+        Y: pd.Series - target variable
+        missing_rate: float - fraction of missing Y
+
+    Returns:
+        X: pd.DataFrame (unchanged)
+        Y_obs: pd.Series - Y with -1 where Y is missing
+    """
+
+    Y_obs = Y.copy()
+    n = len(Y)
+    n_missing = int(n * missing_rate)
+
+    X_array = np.array(X)
+    X_standardized = (X_array - X_array.mean(axis=0)) / (X_array.std(axis=0) + 1e-6)  # standarization (bcs if there is one column with high values it would have bigger influance than the one with smaller values)
+    row_score = X_standardized.sum(axis=1)
+    weights = row_score - row_score.min() + 1e-6
+    probabilities = weights / weights.sum()
+
+    missing_indices = np.random.choice(a=n, size=n_missing, replace=False, p=probabilities)
+    Y_obs.iloc[missing_indices] = -1
+
+    return X, Y_obs
 
 def generate_missing_y(X, Y, scheme, missing_rate):
 
@@ -54,13 +112,13 @@ def generate_missing_y(X, Y, scheme, missing_rate):
         raise ValueError("missing_rate must be a float between 0 and 1.")
 
     if scheme == "MCAR":
-        X, Y_obs = MCAR(X, Y, missing_rate)
+        X, Y_obs = MCAR(X=X, Y=Y, missing_rate=missing_rate)
 
-    if scheme == "MCAR1":
-        raise NotImplementedError("Scheme {scheme} to be implemented.")
+    if scheme == "MAR1":
+        X, Y_obs = MAR1(X=X, Y=Y, missing_rate=missing_rate)
     
-    if scheme == "MCAR2":
-        raise NotImplementedError("Scheme {scheme} to be implemented.")
+    if scheme == "MAR2":
+        X, Y_obs = MAR2(X=X, Y=Y, missing_rate=missing_rate)
     
     if scheme == "MNAR":
         raise NotImplementedError("Scheme {scheme} to be implemented.")
